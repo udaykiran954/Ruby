2aafd50c6de837dd9c1512e21cf442e4e5ae92e5
Mallela Udaykiran <mallelaudaykiran82@gmail.com>
2026-01-21 16:35:01 +0530

    Added Day7 work

--- Day2/demp-app/app/models/customer.rb
+++ Day2/demp-app/app/models/customer.rb
@@ -1,2 +1,18 @@
 class Customer < ApplicationRecord
+
+    def check_email
+        if email.present?
+            true
+        else
+            false
+        end    
+    end
+
+    def name_check
+        if name.match?(/^[a-zA-Z]+$/)
+           puts "Valid name"
+        else
+            puts "Invalid name"
+        end
+    end
 end
--- Day2/demp-app/app/models/product.rb
+++ Day2/demp-app/app/models/product.rb
@@ -1,6 +1,26 @@
 class Product < ApplicationRecord
-    @status  = Product.all.limit(10).pluck(:is_active)
-    @price = Product.all.limit(10).pluck(:price) 
-    @stock = Product.all.limit(10).pluck(:stock) 
-    
+    # @status  = Product.all.limit(10).pluck(:is_active)
+    # @price = Product.all.limit(10).pluck(:price) 
+    # @stock = Product.all.limit(10).pluck(:stock) 
+
+    def check_availability
+        if stock>0
+             true
+        else
+             false
+        end
+    end
+
+    def apply_discount(discount_percent)
+            amount = price + (price*(discount_percent/100))
+        return amount
+    end
+
+    def total_amount
+        if check_availability
+            return apply_discount(10)
+        else
+            return "Product not available"
+        end
+    end 
 end
